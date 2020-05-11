d01b17d5aa6fd19a6e5027e37d3974ef9e6f0f6c
Shakore McGill <mcgills2093@gmail.com>
2020-05-11 15:47:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence1)
   reversed_str = ''
    sentence1.split.each do |i| 
-     reversed_str << #{i
-     i.reverse
+     reversed_str << #{i.reverse}
  end
  sentence1.join('')
 end
